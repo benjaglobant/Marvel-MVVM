47ed389670148ab4b57567068d74969e63cfdbf1
benjaglobant <benjaglobant@users.noreply.github.com>
2020-05-18 11:16:34 -0300

    comments fixed -refactor ids -refactor method

--- app/src/main/java/com/globant/marvelmvvm/view/activity/MainActivity.kt
+++ app/src/main/java/com/globant/marvelmvvm/view/activity/MainActivity.kt
@@ -9,45 +9,45 @@ import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation
 import com.globant.marvelmvvm.R
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
-import kotlinx.android.synthetic.main.activity_main.toolbar
-import kotlinx.android.synthetic.main.activity_main.nav_view
-import kotlinx.android.synthetic.main.activity_main.drawer_layout
+import kotlinx.android.synthetic.main.activity_main.activity_main_toolbar
+import kotlinx.android.synthetic.main.activity_main.activity_main_navigation_view
+import kotlinx.android.synthetic.main.activity_main.activity_main_drawer_layout
 
 class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(activity_main_toolbar)
 
-        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, ZERO, ZERO)
-        drawer_layout.addDrawerListener(toggle)
+        val toggle = ActionBarDrawerToggle(this, activity_main_drawer_layout, activity_main_toolbar, ZERO, ZERO)
+        activity_main_drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener(this)
+        activity_main_navigation_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun showFragment(title: Int, message: Int, fragment: Int){
+        val navController = Navigation.findNavController(this, R.id.activity_main_active_fragment)
+        activity_main_toolbar.title = getString(title)
+        Toast.makeText(this, getString(message), Toast.LENGTH_SHORT).show()
+        navController.navigate(fragment)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val navController = Navigation.findNavController(this, R.id.fragment)
         when (item.itemId) {
             R.id.allCharactersFragment -> {
-                toolbar.title = ALL_CHARACTERS
-                Toast.makeText(this, getString(R.string.string_all_characters_message), Toast.LENGTH_SHORT).show()
-                navController.navigate(R.id.allCharactersFragment)
+                showFragment(R.string.string_all_characters, R.string.string_all_characters_message, R.id.allCharactersFragment)
             }
             R.id.specificCharacterFragment -> {
-                toolbar.title = SPECIFIC_CHARACTER
-                Toast.makeText(this, getString(R.string.string_specific_character_message), Toast.LENGTH_SHORT).show()
-                navController.navigate(R.id.specificCharacterFragment)
+                showFragment(R.string.string_specific_character, R.string.string_specific_character_message, R.id.specificCharacterFragment)
             }
         }
-        drawer_layout.closeDrawer(GravityCompat.START)
+        activity_main_drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
 
     companion object {
-        const val ALL_CHARACTERS = "All Characters"
-        const val SPECIFIC_CHARACTER = "Specific Character"
         const val ZERO = 0
     }
 }
