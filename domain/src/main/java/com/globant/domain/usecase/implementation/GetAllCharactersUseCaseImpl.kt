05f372a1721e26db42f54b5e684c22a97f930756
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-06-09 17:22:31 -0300

    fixed comments

--- domain/src/main/java/com/globant/domain/usecase/implementation/GetAllCharactersUseCaseImpl.kt
+++ domain/src/main/java/com/globant/domain/usecase/implementation/GetAllCharactersUseCaseImpl.kt
@@ -3,16 +3,8 @@ package com.globant.domain.usecase.implementation
 import com.globant.domain.entity.Character
 import com.globant.domain.service.CharactersService
 import com.globant.domain.usecase.GetAllCharactersUseCase
-import com.globant.domain.util.Constants
 import com.globant.domain.util.Result
 
 class GetAllCharactersUseCaseImpl(private val characterService: CharactersService) : GetAllCharactersUseCase {
-    override fun invoke(): Result<List<Character>> {
-        characterService.getAllCharacters().let {
-            return when (it) {
-                is Result.Success -> it
-                is Result.Failure -> Result.Failure(Exception(Constants.NOT_FOUND))
-            }
-        }
-    }
+    override fun invoke(): Result<List<Character>> = characterService.getAllCharacters()
 }
--- domain/src/main/java/com/globant/domain/usecase/implementation/GetSpecificCharacterUseCaseImpl.kt
+++ domain/src/main/java/com/globant/domain/usecase/implementation/GetSpecificCharacterUseCaseImpl.kt
@@ -3,16 +3,8 @@ package com.globant.domain.usecase.implementation
 import com.globant.domain.entity.Character
 import com.globant.domain.service.CharactersService
 import com.globant.domain.usecase.GetSpecificCharacterUseCase
-import com.globant.domain.util.Constants.NOT_FOUND
 import com.globant.domain.util.Result
 
 class GetSpecificCharacterUseCaseImpl(private val characterService: CharactersService) : GetSpecificCharacterUseCase {
-    override fun invoke(characterId: String): Result<List<Character>> {
-        characterService.getSpecificCharacter(characterId).let {
-            return when (it) {
-                is Result.Success -> it
-                is Result.Failure -> Result.Failure(Exception(NOT_FOUND))
-            }
-        }
-    }
+    override fun invoke(characterId: String): Result<List<Character>> = characterService.getSpecificCharacter(characterId)
 }
